3df221d4ce11a5f4af16c10e837fba156cf92702
Vasilii Olekhov <olekhov@gmail.com>
2025-02-17 10:27:15 +0200

    sync with nil

--- lua/basilio/set.lua
+++ lua/basilio/set.lua
@@ -12,10 +12,10 @@ vim.opt.listchars = { tab = "→ ", extends = "", precedes = "", trail = "
 vim.opt.showbreak = "﬌ "
 
 vim.opt.wrap = true
-vim.opt.tabstop = 8
-vim.opt.expandtab = false
-vim.opt.shiftwidth = 8
-vim.opt.softtabstop = 8
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
@@ -23,7 +23,7 @@ vim.opt.ignorecase = true
 
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
+vim.opt.signcolumn = 'yes'
 
 vim.opt.swapfile = false
 vim.opt.backup = false
