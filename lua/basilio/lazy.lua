649db88807b3c593b27d11d68fe65a9a4bfa1084
Vasilii Olekhov <vasilii.olekhov@exness.com>
2025-06-18 05:57:48 +0000

    added chatgpt

--- lua/basilio/lazy.lua
+++ lua/basilio/lazy.lua
@@ -141,6 +141,25 @@ require("lazy").setup({
         }
     },
 
+    -- ChatGPT
+
+    {
+        "jackMort/ChatGPT.nvim",
+        event="VeryLazy",
+        dependencies= {
+            "MunifTanjim/nui.nvim",
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope.nvim',
+        },
+        config = function ()
+            require("chatgpt").setup({
+                api_key_cmd = "pass show api/chatgpt"
+            })
+
+        end
+    },
+
+
     'ethanholz/nvim-lastplace',
 
     -- color scheme
